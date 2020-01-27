7c0e8242410eaea7f2daeaaf51d158c75153699e
Serbest <bunyamin.srbst@gmail.com>
2020-01-27 22:16:04 +0100

    publish

---
+++ build/precache-manifest.9e4f91fd7c39801ebe78ab9168a79cc6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5c172cae97c0f6583dc3d6ce9bb61e",
+    "url": "/Sakalli-Barber-shop/index.html"
+  },
+  {
+    "revision": "554932faead0868cb1cd",
+    "url": "/Sakalli-Barber-shop/static/css/main.61a546f9.chunk.css"
+  },
+  {
+    "revision": "97ba30d2bfdd72a4005b",
+    "url": "/Sakalli-Barber-shop/static/js/2.1ca3bfdd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Sakalli-Barber-shop/static/js/2.1ca3bfdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "554932faead0868cb1cd",
+    "url": "/Sakalli-Barber-shop/static/js/main.c7853bf8.chunk.js"
+  },
+  {
+    "revision": "d623461fab44337f3014",
+    "url": "/Sakalli-Barber-shop/static/js/runtime-main.02abe164.js"
+  },
+  {
+    "revision": "76f4be08f5c042bfba0690a6d7221656",
+    "url": "/Sakalli-Barber-shop/static/media/17.76f4be08.jpg"
+  },
+  {
+    "revision": "73667631f52ddc419a7e7cc2f0fd08c9",
+    "url": "/Sakalli-Barber-shop/static/media/6.73667631.jpg"
+  },
+  {
+    "revision": "c81fa3a826b05862af8f3f84cc93f97f",
+    "url": "/Sakalli-Barber-shop/static/media/Bun-blanc.c81fa3a8.png"
+  },
+  {
+    "revision": "c205ff414fd0cbb2f9c84f15d73ca694",
+    "url": "/Sakalli-Barber-shop/static/media/facebook.c205ff41.svg"
+  },
+  {
+    "revision": "4b950847eb1f26a560ebe3c8221efa47",
+    "url": "/Sakalli-Barber-shop/static/media/google.4b950847.svg"
+  },
+  {
+    "revision": "8b95bf45f8466188c2ec3522907e507b",
+    "url": "/Sakalli-Barber-shop/static/media/tarifs.8b95bf45.png"
+  }
+]);
